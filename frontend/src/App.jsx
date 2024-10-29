00d1eb0c56d1c4ff6e9e1b75a2f1222784652f36
saishanmukh <shanmukhks203@gmail.com>
2024-10-29 20:33:17 +0530

    added card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='flex justify-center h-screen items-center text-4xl'>
-      <Button variant='outline'>Click Me</Button>
-    </div>
+    // <div className='flex justify-center h-screen items-center text-4xl'>
+    //   <Button variant='outline'>Click Me</Button>
+    // </div>
+    <Home></Home>
   )
 }
 
